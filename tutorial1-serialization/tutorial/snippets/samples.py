661b01eb430c7a8d91397667ddae96586c86e47a
한가희 <asgard774@hanyang.ac.kr>
2020-04-12 21:10:24 +0900

    add sample script file

---
+++ tutorial1-serialization/tutorial/snippets/samples.py
@@ -0,0 +1,17 @@
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+
+snippet = Snippet(code='foo = "bar"\n')
+snippet.save()
+
+snippet = Snippet(code='print("hello, world")\n')
+snippet.save()
+
+serializer = SnippetSerializer(snippet)
+
+content = JSONRenderer().render(serializer.data)
+
+print(serializer.data)
+print(content)
